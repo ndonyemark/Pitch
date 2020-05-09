05b3bf4c63075803289841f0047092e11c921bb0
Ndonye Mark <ndonyemark@gmail.com>
2020-05-09 22:17:13 +0300

    initialised configuration files

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from config import config_options
 
 bootstrap=Bootstrap()
 db=SQLAlchemy()
 
-def create_app():
+def create_app(config_name):
 
     app=Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
 
     bootstrap.init_app(app)
     db.init_app(app)
