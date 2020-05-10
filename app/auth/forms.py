13d968897bf39487fefef8a85ee975a0be07bb0e
Ndonye Mark <ndonyemark@gmail.com>
2020-05-10 06:19:28 +0300

    add username and email validation

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,7 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, ValidationError
 from wtforms import StringField, IntegerField, BooleanField, SubmitField,PasswordField
 from wtforms.validators import Email, EqualTo, Required
+from app.models import Users
 
 class Register(FlaskForm):
     username=StringField("Username", validators=[Required()])
@@ -8,3 +9,11 @@ class Register(FlaskForm):
     password=PasswordField("Password", validators=[Required(),EqualTo("confirm_password", message="Passwords do not match")])
     confirm_password=PasswordField("confirm_password", validators=[Required()])
     submit=SubmitField("Register")
+
+    def validate_email(self, data_field):
+        if Users.query.filter_by(email=data_field.data).first():
+            raise ValidationError("There is an account with that email")
+    
+    def validate_username(self, data_field):
+        if Users.query.filter_by(username=data_field.data).first():
+            raise ValidationError("There is an account with that username")
