2f8114a6cea73d7dcd39b5c54bf4adecb4847497
Ndonye Mark <ndonyemark@gmail.com>
2020-05-10 06:10:29 +0300

    added the registration view

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,17 @@
 from . import auth
 from flask import render_template, redirect, request,url_for
 from ..models import Users
+from .forms import Register
+from .. import db
 
 @auth.route("/register")
 def register():
-
+    registration=Register()
+    if registration.validate_on_submit():
+        users=Users(username=registration.username.data, email=registration.email.data, pass_secure=registration.password.data)
+        db.session.add(users)
+        db.session.commit()
+        return redirect("auth.login")
     title="Register"
     return render_template("auth/regiter.html", title=title)
 
