cb2dcc5f7d5127d612f81d9a9ec4a807f4891770
Ndonye Mark <ndonyemark@gmail.com>
2020-05-10 08:27:24 +0300

    added a profile view

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,19 @@
 from . import main
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for, abort
 from flask_login import login_required
+from ..models import Users
 
 @main.route("/")
 def index():
 
     title="Pitch"
     return render_template("index.html", title=title)
+
+@main.route("/user/<uname>")
+def profile(uname):
+    user=Users.query.filter_by(username=user).first()
+
+    if user is None:
+        abort(404)
+    
+    return render_template("profile/profile.html")
