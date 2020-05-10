86ebd14610510f415428ce17b3bb03e74fdd074e
Ndonye Mark <ndonyemark@gmail.com>
2020-05-10 05:42:08 +0300

    forms.py correction

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, SubmitField,PasswordField
 from wtforms.validators import Email, EqualTo, Required
 
-class Register():
+class Register(FlaskForm):
     username=StringField("Username", validators=[Required()])
     email=StringField("Email", validators=[Required(), Email()])
     password=PasswordField("Password", validators=[Required(),EqualTo("confirm_password", message="Passwords do not match")])
